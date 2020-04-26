8bb3407089ef00635ab0ac14617d5b7da3c27994
Carl Dungca <carl.dungca@yahoo.com>
2020-04-26 13:34:09 -0700

    Seat status bar now changes color and size based on seat capacity

--- src/components/longTermPlanner/DriverCard.js
+++ src/components/longTermPlanner/DriverCard.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PassengerTile from './PassengerTile';
 
 const DriverCard = ({name, passengers, totalSeats}) => {
+	// To conditionally update the progress bar's width styling
+	const [seatCapacityBar, updateCapacityBar] = useState(0);
+	useEffect(() => updateCapacityBar((passengers.length / totalSeats) * 100));
+
 	return (
 		<div className="driversCard">
 			<div className="driversCard__header">
 				<p>{name}</p>
 				<p>edit</p>
 				<p>({passengers.length}/{totalSeats})</p>
-				<div className="driversCard__percentBar"></div>
+				<div 
+					className={`${seatCapacityBar} ` < 100 ? 'driversCard__percentBar--incomplete' : 'driversCard__percentBar--complete' }
+					style={{
+						width: `${seatCapacityBar}%`,
+					}}>
+				</div>
 			</div>
 			<div className="passengerTiles">
 				{passengers.map((passenger, i) => 
