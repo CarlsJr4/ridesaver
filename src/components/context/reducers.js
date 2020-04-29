24abf5353047a39e1f2edaaa4f3a0c1a0be9ccd2
Carl Dungca <carl.dungca@yahoo.com>
2020-04-28 22:12:36 -0700

    Refactored DriverStatusBar component to use reducer hook for simplicity

--- src/components/context/reducers.js
+++ src/components/context/reducers.js
@@ -20,6 +20,45 @@ export function driverReducer(state, action) {
 	}
 }
 
+export function statusReducer(state, action) {
+	const drivers = action.drivers;
+	switch(action.type) {
+		// This is all the math needed to generate the statistics bar
+		// Is there any way we can shorten it?
+		case 'UPDATE':
+			const reducer = (accumulator, currentValue) => accumulator + currentValue;
+			let usedSeats = [];
+			let maxSeats = [];
+			let usedSeatCount = 0;
+			let emptyCars = true;
+			drivers.forEach(driver => {
+				usedSeats.push(...driver.passengers)
+				maxSeats.push(driver.seats)
+			});
+	
+			maxSeats = maxSeats.reduce(reducer);
+			usedSeatCount = usedSeats.length;
+
+			for (let i = 0; i < drivers.length; i++) {
+				if (drivers[i].passengers.length === 0) {
+					emptyCars = true;
+					break;
+				} else {
+					emptyCars = false;
+				}
+			}
+
+			return {
+				usedSeatCount,
+				maxSeats,
+				emptyCars
+			}
+
+		default:
+			throw new Error();
+	}
+}
+
 export function passengerReducer(state, action) {
 	let passengers = [...state];
 	switch (action.type) {
--- src/components/longTermPlanner/driverPanel/DriverStatusBar.js
+++ src/components/longTermPlanner/driverPanel/DriverStatusBar.js
@@ -1,55 +1,26 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext, useReducer } from 'react';
 import { CarpoolContext } from '../../context/GlobalState';
+import { statusReducer } from '../../context/reducers';
 
 const DriverStatusBar = () => {
-	const [seatStatus, updateSeatStatus] = useState({});
-	const [emptyCars, setEmptyCars] = useState(true);
+	const [status, updateStatus] = useReducer(statusReducer, {
+		usedSeatCount: 0,
+		maxSeats: 0,
+		emptyCars: false
+	})
 
 	const {driverList, passengerList} = useContext(CarpoolContext);
 
-	useEffect(() => {
-		updateSeatStatus(calcSeats(driverList));
-
-		for (let i = 0; i < driverList.length; i++) {
-			if (driverList[i].passengers.length === 0) {
-				setEmptyCars(true);
-				break;
-			} else {
-				setEmptyCars(false);
-			}
-		}
-	}, [driverList]);
-
-	// Should we move this to a util folder?
-	// If we want to make this function reusable, we need to make it more generic
-	const calcSeats = (drivers) => {
-		const reducer = (accumulator, currentValue) => accumulator + currentValue;
-		let usedSeats = [];
-		let maxSeats = [];
-		let usedSeatCount = 0;
-		
-		drivers.forEach(driver => {
-			usedSeats.push(...driver.passengers)
-			maxSeats.push(driver.seats)
-		});
-
-		maxSeats = maxSeats.reduce(reducer);
-		usedSeatCount = usedSeats.length;
-
-		return {
-			usedSeatCount,
-			maxSeats,
-		}
-	}
+	useEffect(() => updateStatus({type: 'UPDATE', drivers: driverList}), [driverList]);
 
 	return (
 		<ul className="drivers__statusBar">
 			<li>
-				<strong>{seatStatus.usedSeatCount}/{seatStatus.maxSeats} </strong>
+				<strong>{status.usedSeatCount}/{status.maxSeats} </strong>
 				seats used
 			</li>
 			<li>
-				{emptyCars ? 'There are empty cars' : 'No empty cars'}
+				{status.emptyCars ? 'There are empty cars' : 'No empty cars'}
 			</li>
 			<li>
 				<strong>{passengerList.length} </strong>
