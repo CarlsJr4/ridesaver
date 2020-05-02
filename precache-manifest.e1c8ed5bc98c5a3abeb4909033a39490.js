69f4cbc98f84808a1547907ec213f86b84dc71f1
Carl Dungca <carl.dungca@yahoo.com>
2020-05-01 19:52:31 -0700

    Updates

---
+++ precache-manifest.e1c8ed5bc98c5a3abeb4909033a39490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c7ba1935edc70ea47dea4182e912a2",
+    "url": "/carpool-manager/index.html"
+  },
+  {
+    "revision": "7f8c529daad6d0411231",
+    "url": "/carpool-manager/static/css/main.ed07252c.chunk.css"
+  },
+  {
+    "revision": "14c88a899706b82ee77d",
+    "url": "/carpool-manager/static/js/2.5d746db8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/carpool-manager/static/js/2.5d746db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8c529daad6d0411231",
+    "url": "/carpool-manager/static/js/main.85995375.chunk.js"
+  },
+  {
+    "revision": "368f593218456c73865b",
+    "url": "/carpool-manager/static/js/runtime-main.29d7a4bf.js"
+  }
+]);
