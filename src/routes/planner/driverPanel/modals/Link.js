fbdb668a777b9f9da15909482eb91fc9462d34f0
Carl Dungca <carl.dungca@yahoo.com>
2020-05-12 12:11:55 -0700

    Added basic responsive styling

--- src/routes/planner/driverPanel/modals/Link.js
+++ src/routes/planner/driverPanel/modals/Link.js
@@ -9,9 +9,9 @@ const LinkModal = ({isVisible, handleVisibility}) => {
 		>
 			<div className="modalMessage">
 				<h1>Sit back and relax.</h1>
+				<strong><a href="#">https://thisfeatureisntreadyyet.com/work-in-progress</a></strong>
 				<p>Share this link with your attendees to allow them to sign up on their own!</p>
 				<p>No account needed.</p>
-				<h3><a href="#">https://thisfeatureisntreadyyet.com/work-in-progress</a></h3>
 				<p>They can sign up as a driver or passenger to an existing car.</p>
 			</div>
 		</Modal>
