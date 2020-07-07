de40d09daa693b7e8462324ce08985d107e3ed0e
Carl Dungca <carl.dungca@yahoo.com>
2020-07-06 19:35:05 -0700

    Rewrote the unassignedContainer to correctly map passenger pool data

--- src/routes/planner/passengerPanel/UnassignedContainer.js
+++ src/routes/planner/passengerPanel/UnassignedContainer.js
@@ -6,43 +6,48 @@ import IconButton from '../../../reusable_components/IconButton';
 import AddPassengerModal from './AddPassengerModal';
 
 const Passengers = () => {
-	const { updateDriverList, driverList } = useContext(CarpoolContext);
-	const [isAddingPassenger, toggleAddPassenger] = useState(false);
+  const { updateDriverList, driverList } = useContext(CarpoolContext);
+  const [isAddingPassenger, toggleAddPassenger] = useState(false);
+  const drivers = driverList.driverColumns;
 
-	const passengerIds = driverList.driverColumns.unassignedPassengers.passengerIds;
-	const passengerData = passengerIds.map(id => driverList.passengerRows[id]);
-	const columnId = "unassignedPassengers";
+  let passengerPoolId;
 
-	return ( 
-		<div className="passengersContainer">
-			<AddPassengerModal
-				isVisible={isAddingPassenger}
-				handleVisibility={toggleAddPassenger}
-			/>
-			<div className="passengersContainer__header">
-				<h3>Unassigned Riders</h3>
-				<IconButton 
-					icon="plus"
-					handleClick={() => toggleAddPassenger(true)}	
-				/>
-			</div>
-				<Droppable
-					droppableId={columnId}
-				>
-					{(provided, snapshot) => (
-						<PassengerContainer 
-							{...provided.droppableProps}
-							innerRef={provided.innerRef}
-							handleUpdate={updateDriverList}
-							passengers={passengerData}
-							placeholder={provided.placeholder}
-							driverId={columnId}
-							isDraggingOver={snapshot.isDraggingOver}
-						/>
-					)}
-				</Droppable>
-		</div>
-	);
-}
+  for (const driverId in drivers) {
+    if (drivers[driverId].isPassengerPool) {
+      passengerPoolId = driverId;
+    }
+  }
+
+  const passengerIds = driverList.driverColumns[passengerPoolId].passengerIds;
+
+  const passengerData = passengerIds.map(id => driverList.passengerRows[id]);
+  const columnId = passengerPoolId;
+
+  return (
+    <div className="passengersContainer">
+      <AddPassengerModal
+        isVisible={isAddingPassenger}
+        handleVisibility={toggleAddPassenger}
+      />
+      <div className="passengersContainer__header">
+        <h3>Unassigned Riders</h3>
+        <IconButton icon="plus" handleClick={() => toggleAddPassenger(true)} />
+      </div>
+      <Droppable droppableId={columnId}>
+        {(provided, snapshot) => (
+          <PassengerContainer
+            {...provided.droppableProps}
+            innerRef={provided.innerRef}
+            handleUpdate={updateDriverList}
+            passengers={passengerData}
+            placeholder={provided.placeholder}
+            driverId={columnId}
+            isDraggingOver={snapshot.isDraggingOver}
+          />
+        )}
+      </Droppable>
+    </div>
+  );
+};
 
 export default Passengers;
